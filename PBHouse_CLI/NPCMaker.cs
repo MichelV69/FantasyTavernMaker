3d8740301797937e50df788896e1af8a2da20b3d
Michel R Vaillancourt <michel.vaillancourt@thinkingbig.net>
2019-11-22 07:32:13 -0400

    Basic NPC attributes List
    Core attributes I think are required for interesting NPCs in a Fantasy Story environment.

--- PBHouse_CLI/NPCMaker.cs
+++ PBHouse_CLI/NPCMaker.cs
@@ -2,16 +2,24 @@ namespace PBHouse_CLI
 {
   class NPCMaker
   {
-      private int TypeCode;
-      private static string[] TypeCodeList = {"Staff", "Warrior", "Spell-Caster", "Noble", "Merchant"};
-      private string Race;
-      private int GenderCode;
-      private static string[] GenderCodeList = {"Male", "Female", "Androgenous"}
-      private string HeightDesc;
-      private string BuildDesc;
-      private string HairColor;
-      private string HairStyle;
-      private string EyeColor;
 
+      private static string[] TypeCodeList = {"Staff", "Warrior", "Spell-Caster", "Noble", "Merchant"};
+      private static string[] GenderCodeList = {"Male", "Female", "Androgenous"};
+      private static string[] SexualOrrientationCodeList = {"Hetro", "Bi", "Gay", "Asex"};
+      private int TypeCode { get; set; }
+      private int SexualOrrientationCode { get; set; }
+      private int GenderCode { get; set; }
+      private string NameDesc { get; set; }
+      private string Race { get; set; }
+      private string HeightDesc { get; set; }
+      private string BuildDesc { get; set; }
+      private string HairColor { get; set; }
+      private string HairStyle { get; set; }
+      private string EyeColor { get; set; }
+      private string QuirkEmotional { get; set; }
+      private string QuirkPhysical { get; set; }
+      private string NotableAttributePositive { get; set; }
+      private string NotableAttributeNegative { get; set; }
+      private string SchtickAbilityDescription { get; set; }
   }
 }
