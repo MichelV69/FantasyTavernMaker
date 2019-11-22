623658343c26ac45c30c1c3dd764cc8fd54f247a
Michel R Vaillancourt <michel.vaillancourt@thinkingbig.net>
2019-11-22 07:08:47 -0400

    New Class - NPCMaker
    Start setting up the class skeleton to support random generation of bar staff, patrons, etc.

---
+++ PBHouse_CLI/NPCMaker.cs
@@ -0,0 +1,17 @@
+namespace PBHouse_CLI
+{
+  class NPCMaker
+  {
+      private int TypeCode;
+      private static string[] TypeCodeList = {"Staff", "Warrior", "Spell-Caster", "Noble", "Merchant"};
+      private string Race;
+      private int GenderCode;
+      private static string[] GenderCodeList = {"Male", "Female", "Androgenous"}
+      private string HeightDesc;
+      private string BuildDesc;
+      private string HairColor;
+      private string HairStyle;
+      private string EyeColor;
+
+  }
+}
