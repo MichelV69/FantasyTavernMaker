f6cdc44c7b59def0a6891b0c030ee47f56639ab3
Michel R Vaillancourt <michel.vaillancourt@thinkingbig.net>
2019-11-25 11:48:56 -0400

    Improve total randomness of rolls

--- PBHouse_CLI/DiceRollerEngine.cs
+++ PBHouse_CLI/DiceRollerEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 /*
  * First created Tue 27-Aug-2019 @ 09h42 ADT by m.vaillancourt
@@ -9,7 +10,30 @@ namespace PBHouse_CLI
 {
     public class DiceBagEngine
     {
-        public static Random rndGen = new Random((int)DateTime.Now.Ticks);
+        // public static Random rndGen = new Random((int)DateTime.Now.Ticks);
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        // -----
+        private static int getRandomNumber(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Math.Max, and substracting 0.00000000001,
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
+        }
 
         // -----
         public int RollDice(string dice_string)
@@ -52,7 +76,7 @@ namespace PBHouse_CLI
                 total_roll = 0;
                 for (int loop = 1; loop <= number_of_rolls; loop++)
                 {
-                    total_roll = total_roll + rndGen.Next(1, type_of_dice);
+                    total_roll = total_roll + getRandomNumber(1, type_of_dice);
                 } // end-for
 
                 // add roll_mod to total_roll
